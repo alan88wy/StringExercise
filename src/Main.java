776a4a272b345c7d4cb3fc95947295b51ec1788e
Alan <alan88w@yahoo.com>
2020-09-27 22:43:22 +0800

    Second Commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class Main {
+
+  public static void main(String[] args) {
+    var doubleValue = 10_000_000.53;
+
+    var numF = NumberFormat.getNumberInstance();
+
+    System.out.println("Number : " + numF.format(doubleValue));
+
+    var intF = NumberFormat.getIntegerInstance();
+    System.out.println("Integer : " + intF.format(doubleValue));
+
+    var locale = Locale.getDefault();
+    var localeFormater = NumberFormat.getIntegerInstance(locale);
+
+    System.out.println("Number - local : " + localeFormater.format(doubleValue));
+
+    var currencyF = NumberFormat.getCurrencyInstance();
+
+    System.out.println("Currency Format : " + currencyF.format(doubleValue));
+
+  }
+}
